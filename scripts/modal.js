ef9c37a0e7277798e28b3f9e9454f6862446f5bf
juanigm <juanigm99@gmail.com>
2022-03-26 15:00:59 -0300

    Modal Ready

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,5 +1,6 @@
-const videos = document.querySelector('#asdasd');
+const videos = document.querySelector('.ase');
 const modal = document.querySelector('.modal');
+const iframe = document.querySelector('.testa');
 const modalContainer = document.querySelector('.modal__content');
 const close = document.querySelector('.btn-close');
 const bg = document.querySelector(".modal-bg");
@@ -7,6 +8,8 @@ const bg = document.querySelector(".modal-bg");
 videos.addEventListener("click", () => {
     modal.classList.add("modal-open");
     bg.classList.add("modal-open");
+    let yt = videos.getAttribute("id")
+    iframe.setAttribute("src", yt);
 });
 
 let num = videos.length;
