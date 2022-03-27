e1e1173953b0f4347c0e082fe9fdcc950af5ab62
juani <juanigm99@gmail.com>
2022-03-27 20:08:06 -0300

    Modal Finished

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,22 +1,30 @@
-const videos = document.querySelector('.ase');
+const videos = document.querySelectorAll('.video-item');
 const modal = document.querySelector('.modal');
-const iframe = document.querySelector('.testa');
+const iframe = document.querySelector('.modal-video');
 const modalContainer = document.querySelector('.modal__content');
 const close = document.querySelector('.btn-close');
 const bg = document.querySelector(".modal-bg");
 
-videos.addEventListener("click", () => {
-    modal.classList.add("modal-open");
-    bg.classList.add("modal-open");
-    let yt = videos.getAttribute("id")
-    iframe.setAttribute("src", yt);
+videos.forEach((video) => {
+    video.addEventListener("click", () => {
+        modal.classList.add("modal-open");
+        bg.classList.add("modal-open");
+        let yt = video.getAttribute("id") + '?autoplay=1&loop=1';
+        iframe.setAttribute("src", yt);
+    });
 });
 
-let num = videos.length;
-console.log(num);
-
-
-close.addEventListener("click", () => {
+const closeModal = () => {
     modal.classList.remove("modal-open");
+    iframe.removeAttribute("src");
     bg.classList.remove("modal-open");
+}
+
+close.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", e => {
+    if(e.key == "Escape"){
+        closeModal();
+    }
 });
+
