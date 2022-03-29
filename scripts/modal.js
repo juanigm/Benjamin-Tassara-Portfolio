dc79c4b3ba3bade3e14aab5449fa3a2ee1db4be9
juani <juanigm99@gmail.com>
2022-03-29 12:36:40 -0300

    Finishing Web Page
    I finished de modal window, added paragraph styles in modal window. Fixed responsive problems with IOS and fixed responsive font-size

--- scripts/modal.js
+++ scripts/modal.js
@@ -4,13 +4,17 @@ const iframe = document.querySelector('.modal-video');
 const modalContainer = document.querySelector('.modal__content');
 const close = document.querySelector('.btn-close');
 const bg = document.querySelector(".modal-bg");
+const p = document.querySelector(".p-modal");
 
 videos.forEach((video) => {
     video.addEventListener("click", () => {
         modal.classList.add("modal-open");
         bg.classList.add("modal-open");
         let yt = video.getAttribute("id") + '?autoplay=1&loop=1';
+        let desc = video.getAttribute("description");
         iframe.setAttribute("src", yt);
+        p.textContent = desc;
+        
     });
 });
 
